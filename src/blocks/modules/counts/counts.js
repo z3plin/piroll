be5fc10ad8e7259e6c81b489ad825800870c398c
z3plin <serj.raguzin@yandex.ru>
2020-10-09 15:51:03 +0300

    update count for counts section when it in a view

--- src/blocks/modules/counts/counts.js
+++ src/blocks/modules/counts/counts.js
@@ -6,16 +6,30 @@ const options = {
     duration: 4,
 };
 
-let projects = new CountUp('countProjects', 548, options);
-let hours = new CountUp('countHours', 1465, options);
-let feedbacks = new CountUp('countFeedbacks', 612, options);
-let clients = new CountUp('countClients', 735, options);
+let countsSection = document.getElementById('counts');
 
-if (!projects.error && !hours.error && !feedbacks.error && !clients.error) {
+window.addEventListener('scroll', function() { 
+    isScrolledIntoView(countsSection);
+    let projects = new CountUp('countProjects', 548, options);
+    let hours = new CountUp('countHours', 1465, options);
+    let feedbacks = new CountUp('countFeedbacks', 612, options);
+    let clients = new CountUp('countClients', 735, options);
     projects.start();
     hours.start();
     feedbacks.start();
     clients.start();
-} else {
-    console.error(demo.error);
-}
+    
+    // if (!projects.error && !hours.error && !feedbacks.error && !clients.error && isScrolledIntoView(countsSection)) {
+    // } else {
+    //     console.log(countsSection.getBoundingClientRect());
+    // };
+});
+
+function isScrolledIntoView(countsSection) {
+    var rect = countsSection.getBoundingClientRect();
+    var elemTop = rect.top;
+    var elemBottom = rect.bottom;
+
+    var isVisible = (elemTop >= 0) && (elemBottom <= window.innerHeight);
+    return isVisible; 
+};
