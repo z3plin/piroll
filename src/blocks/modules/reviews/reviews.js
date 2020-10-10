ea2ba2a39e97d326a9b213c5dcc31230485d9902
z3plin <serj.raguzin@yandex.ru>
2020-10-10 22:27:17 +0300

    reviews section

--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -1,10 +1,19 @@
-import Splide from '@splidejs/splide';
+import Swiper, { Autoplay, EffectFlip, Pagination } from 'swiper';
 
-document.addEventListener( 'DOMContentLoaded', function () {
-	new Splide( '#reviewsSplide', {
-        type: 'loop',
-        width: '100vw',
-        height: '100vh',
-        autoplay: true,
-	} ).mount();
-} ); 
+Swiper.use([Pagination, Autoplay, EffectFlip]);
+
+const swiper = new Swiper('.reviews__slider', {
+        pagination: {
+                el: '.swiper-pagination',
+                dynamicBullets: true,
+                clickable: true,
+        },
+        loop: true,
+        slidesPerView: 'auto',
+        effect: 'flip',
+        speed: 800,
+        autoplay: {
+                delay: 3000,
+        },
+        observer: true,
+});
