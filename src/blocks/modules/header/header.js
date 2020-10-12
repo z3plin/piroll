92ed4c6d014e3995bc6909b3bef48f223df46a4e
Serge <serj.raguzin@yandex.ru>
2020-10-12 14:23:29 +0300

    update

--- src/blocks/modules/header/header.js
+++ src/blocks/modules/header/header.js
@@ -1,7 +1,29 @@
+const body = document.body;
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".mobile__menu-list");
 const links = document.querySelectorAll(".mobile__menu-list li");
 const logo = document.querySelector(".header__logo");
+const triggerMenu = document.querySelector(".trigger");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
 
 hamburger.addEventListener("click", () => {
   logo.classList.toggle("open");
--- src/blocks/modules/reviews/reviews.js
+++ src/blocks/modules/reviews/reviews.js
@@ -8,12 +8,21 @@ const swiper = new Swiper('.reviews__slider', {
                 dynamicBullets: true,
                 clickable: true,
         },
+        // autoHeight: true,
         loop: true,
         slidesPerView: 'auto',
         effect: 'flip',
-        speed: 800,
+        speed: 1200,
         autoplay: {
                 delay: 3000,
         },
         observer: true,
+        breakpoints: {
+                320: {
+                  autoHeight: true,
+                },
+                576: {
+                  autoHeight: false,
+                },
+        },
 });
